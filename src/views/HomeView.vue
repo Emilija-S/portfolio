4d64a2f993f6392cb2372e3b0ba7d13d2e7d009d
Emilija-S <emilija.simonoska@hotmail.com>
2022-07-04 19:51:39 +0200

    CV page designed

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,79 @@
-<template>
-  <main>
+<template >
+  <main class="container mt-5">
+    <section class="mb-3">
+      <div class="row">
+        <div class="col-sm-8 col-md-2 justify-content-center">
+          <div>
+            <img src="../assets/images/xconvert.com.webp" alt="Profile image">
+          </div>
+        </div>
+        <div class="col-4 align-middle">
+          <h3 style="font-weight: bold; color: brown">Emilija Simonoska</h3>
+          <h5>Full Stack Developer</h5>
+        </div>
+      </div>
+    </section>
 
+    <section class="container">
+      <div class="row">
+        <div class="col-9">
+          <div id="PersonalStatement" class="pt-3">
+            <h5 style="font-weight: bold; color: brown"><i class="fa fa-user m-1" aria-hidden="true"></i>Personal Statement</h5>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+          </div>
+
+          <div id="education" class="pt-3">
+
+            <h5 style="color: brown; font-weight: bold" class="pt-4"><i class="fa fa-university" aria-hidden="true"></i>Ss. Cyril and Methodius University in Skopje </h5>
+            <p style="font-weight: normal;" class="m-1">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and Lorem Ipsum  </p>
+
+          </div>
+
+          <div id="employmentHistory" class="pt-3">
+
+            <h5 style="color: brown; font-weight: bold" class="pt-4"><i class="fa fa-briefcase" aria-hidden="true"></i>Employment History</h5>
+            <p style="font-weight: normal;" class="m-1">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and Lorem Ipsum Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. </p>
+
+          </div>
+        </div>
+
+        <div class="col text-start">
+          <h4 style="font-weight: bold; color: brown;">Interests</h4>
+          <h6 style="color: grey">Books</h6>
+          <h6 style="color: grey">Technology</h6>
+          <h6 style="color: grey">Camping</h6>
+          <h6 style="color: grey">Photography</h6>
+          <h6 style="color: grey">Writing</h6>
+          <h6 style="color: grey">Travel</h6>
+          <h6 style="color: grey">Tennis</h6>
+          <h6 style="color: grey">.</h6>
+          <h6 style="color: grey">.</h6>
+          <h6 style="color: grey">.</h6>
+
+
+
+          <h4 style="font-weight: bold; color: brown">Skills</h4>
+          <h6 style="color: grey">HTML</h6>
+          <h6 style="color: grey">CSS</h6>
+          <h6 style="color: grey">JAVASCRIPT</h6>
+          <h6 style="color: grey">JAVA</h6>
+          <h6 style="color: grey"></h6>
+          <h6 style="color: grey">.</h6>
+          <h6 style="color: grey">.</h6>
+          <h6 style="color: grey">.</h6>
+
+        </div>
+      </div>
+    </section>
+    <section class="container pt-1 mb-4">
+      <div class="row">
+          <h5 class="col-1"><i class="fa fa-phone" aria-hidden="true"></i> </h5>
+          <h5 class="col-1"><i class="fa fa-envelope" aria-hidden="true"></i></h5>
+          <h5 class="col-1"><i class="fa fa-github" aria-hidden="true"></i></h5>
+        <h5 class="col-1"><i class="fa fa-quora" aria-hidden="true"></i></h5>
+
+        </div>
+    </section>
   </main>
 </template>
 
@@ -12,5 +85,10 @@ export default {
 </script>
 
 <style scoped>
+
+img {
+  width: 170px;
+  height: 190px;
+}
 
 </style>
